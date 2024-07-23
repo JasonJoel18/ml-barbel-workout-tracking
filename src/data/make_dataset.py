8c647816cda6e2612399a10243b8d857f32e84e8
JasonJoel18 <159021390+JasonJoel18@users.noreply.github.com>
2024-07-23 04:10:44 +0200

    v0.7.23.4
    Extract features from filename | Reading all files | Working with
    Datetime

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -80,12 +80,22 @@ for f in files:
 # --------------------------------------------------------------
 acc_df.info()
 pd.to_datetime(df['epoch (ms)'], unit="ms")
-pd.to_datetime(df['time (01:00)'])
 
+acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit="ms")
+
+del acc_df["epoch (ms)"]
+del acc_df["time (01:00)"]
+del acc_df["elapsed (s)"]
+
+del gyr_df["epoch (ms)"]
+del gyr_df["time (01:00)"]
+del gyr_df["elapsed (s)"]
 
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+
 
 
 # --------------------------------------------------------------
