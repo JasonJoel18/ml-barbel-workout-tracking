b4b59b1e7ee30ee99171a3e0602b0c8cf6f34f92
JasonJoel18 <159021390+JasonJoel18@users.noreply.github.com>
2024-07-23 05:35:13 +0200

    v0.7.23.6
    Removed the print command

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -21,13 +21,25 @@ files = glob(
 data_path = "../../data/raw/MetaMotion/"
 
 acc_df, gyr_df = functions.read_data_from_files(files,data_path)
-print(acc_df)
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
 
 
+data_merged = pd.concat([acc_df.iloc[:,:3],gyr_df],axis=1)
+data_merged.columns = [
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "participant",
+    "label",
+    "category",
+    "set"
+]
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
 # --------------------------------------------------------------
@@ -35,7 +47,32 @@ print(acc_df)
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+sampling = {
+    "acc_x" : "mean",
+    "acc_y" : "mean",
+    "acc_z" : "mean",
+    "gyr_x" : "mean",
+    "gyr_y" : "mean",
+    "gyr_z" : "mean",
+    "participant" : "last",
+    "label" : "last",
+    "category" : "last",
+    "set" : "last"
+}
 
+
+data_merged[:100].resample(rule="200ms").apply(sampling)
+
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+days[1]
+
+data_resampled = pd.concat([df.resample(rule="200ms").apply(sampling).dropna() for df in days])
+
+data_resampled["set"] = data_resampled["set"].astype("int")
+
+data_resampled.info()
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
